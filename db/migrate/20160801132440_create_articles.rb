250c1573ece491d48dde8fd003ef2645a1e0988f
rustemmukhamadiev <rustem.mukhamadiev@flatstack.com>
2016-08-01 18:40:06 +0300

    add migrations

---
+++ db/migrate/20160801132440_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title, null: false
+      t.text :text, null: false
+
+      t.references :user, index: true, null: false, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
