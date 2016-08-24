2ac068ea0fbb91bd06566bc66a13316743e16e49
rustemmukhamadiev <rustem.mukhamadiev@flatstack.com>
2016-08-24 18:31:38 +0300

    update avatar validation and production ENV

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style/missing_photo.png"
 
-  validates_attachment_content_type :avatar, content_type: %r{/\Aimage\/.*\Z/}
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\Z}
 end
