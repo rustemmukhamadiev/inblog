ac2338d9b6679a8e7f3ff2e0a538a0e1f9722dd6
rustemmukhamadiev <rustem.mukhamadiev@flatstack.com>
2016-09-07 13:03:33 +0300

    update decorators

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -1,6 +1,4 @@
 class ArticleDecorator < ApplicationDecorator
-  decorates_association :user
-
   delegate :title, :text
   delegate :thumb_avatar, to: :user, prefix: true
 
@@ -9,10 +7,20 @@ class ArticleDecorator < ApplicationDecorator
   end
 
   def posted_by
-    "#{user.full_name} | #{h.time_ago_in_words(object.created_at)} ago"
+    "#{user.full_name} | #{formatted_created_at} ago"
+  end
+
+  def formatted_created_at
+    h.time_ago_in_words(object.created_at)
   end
 
   def comment_count
     "Сomments: #{object.comments.count}"
+  end
+
+  private
+
+  def user
+    @_user ||= object.user.decorate
   end
 end
--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,14 +1,18 @@
 class CommentDecorator < ApplicationDecorator
-  decorates_association :user
-
   delegate :text
   delegate :thumb_avatar, to: :user, prefix: true
 
   def posted_by
-    "#{user.full_name} | #{formatted_created_at}"
+    "#{user.full_name} | #{formatted_created_at} ago"
   end
 
   def formatted_created_at
     h.time_ago_in_words(object.created_at)
+  end
+
+  private
+
+  def user
+    @_user ||= object.user.decorate
   end
 end
