5ae11e55e3e2eaa8db9b14e59e2218fb11720e48
rustemmukhamadiev <rustem.mukhamadiev@flatstack.com>
2016-09-14 17:55:54 +0300

    update design

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,6 +1,6 @@
 class CommentDecorator < ApplicationDecorator
   delegate :text
-  delegate :thumb_avatar, to: :user, prefix: true
+  delegate :thumb_avatar, :username, :full_name, to: :user, prefix: true
 
   def posted_by
     "#{user.full_name} | #{formatted_created_at} ago"
@@ -8,12 +8,6 @@ class CommentDecorator < ApplicationDecorator
 
   def formatted_created_at
     h.time_ago_in_words(object.created_at)
-  end
-
-  def username_link
-    h.link_to(h.username_articles_path(user.username)) do
-      (user.full_name + " | " + formatted_created_at + " ago").html_safe
-    end
   end
 
   private
