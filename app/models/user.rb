7c93e47131811645ff0cfdfaae8d363c35216aeb
rustemmukhamadiev <rustem.mukhamadiev@flatstack.com>
2016-10-03 13:09:35 +0300

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,9 @@ class User < ActiveRecord::Base
   validates :username,
     presence: true,
     uniqueness: true,
-    format: { with: /\A[a-z]+[a-z]+\z/, message: "only allows letters" }
+    format: { with: /\A[A-Za-z]+[A-Za-z]+\z/, message: "only allows letters" }
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style/missing_photo.png"
 
   validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\Z}
-
-  private
-
-  def downcase_username
-    username.downcase! if username.present?
-  end
 end
